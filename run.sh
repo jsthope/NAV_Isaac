2fc54d9d23e3bed24a175d9d530a2e37cf1188d6
jsthope <jsthope@users.noreply.github.com>
2026-03-28 19:52:37 +0100

    fix sensor

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 cd isaac/src/isaac_ros_common && ./scripts/run_dev.sh -i ros2_humble.zed -a "-v /usr/local/zed/settings:/usr/local/zed/settings \
-    -v /usr/local/zed/resources:/usr/local/zed/resources"
+    -v /usr/local/zed/resources:/usr/local/zed/resources \
+    -v /dev/bus/usb:/dev/bus/usb \
+    --device /dev/hidraw2:/dev/hidraw2"
