5b1d75cb491f64c461a5e742889d4a5dfff6ed65
jsthope <jsthope@users.noreply.github.com>
2026-04-02 19:32:06 +0200

    fix

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 just need to edit isaac/src/isaac_ros_common/docker/Dockerfile.zed for AMD/ARM
+after ./run.sh leave this shell alone (for no fucking reason but he will be cursed so work with new ones)
+in a new shell the exec a.txt 
+then it's cool
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 cd isaac/src/isaac_ros_common && ./scripts/run_dev.sh -i ros2_humble.zed -a "-v /usr/local/zed/settings:/usr/local/zed/settings \
     -v /usr/local/zed/resources:/usr/local/zed/resources \
     -v /dev/bus/usb:/dev/bus/usb \
+    -v /dev:/dev \
+    -e DISPLAY=$DISPLAY \
     --device /dev/hidraw2:/dev/hidraw2"
